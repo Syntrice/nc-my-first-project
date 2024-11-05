1903fbee16adae988be1fc5413b77ff65bd7846b
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-11-05 10:21:41 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NorthcodersMyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
