b14ff69b3540c3a5fd7871427079ebfea9449958
syntrice <76839403+Syntrice@users.noreply.github.com>
2024-11-05 10:29:38 +0000

    Include and print favourite film information

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@
         {
             string name = "Sam";
             int age = 23;
+            string favouriteFilm = "Interstellar";
+
 
             Console.WriteLine("Hello, my name is: " + name);
             Console.WriteLine("My age is: " + age);
+            Console.WriteLine("My favourite film is: " + favouriteFilm);
         }
     }
 }
